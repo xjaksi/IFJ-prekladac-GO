6e74324fc3d058efed87d52e69559f9e30f0d9bd
xvlasa14 <48327260+xvlasa14@users.noreply.github.com>
2020-09-24 10:50:10 +0200

    Update errors.h
    přidány chybové hodnoty

--- src/errors.h
+++ src/errors.h
@@ -17,15 +17,18 @@
 #include <stdlib.h>
 
 typedef enum error_code{
-    NOERROR_OK = 0,
-    ERROR_LEX = 1,
-    ERROR_SYN = 2,
-    ERROR_SEM_VAR = 3,  // neefinovana funkce/promenna, pokus o redefinici
-    ERROR_SEM_FUN = 4,  // chyba typove kompatibility
-    ERROR_SEM_ARG = 5,  // spatny pocet argumentu
-    ERROR_SEM = 6,
-    ERROR_ZERO_DIV = 9,
-    ERROR_COMPILATOR = 99,
+	OK = 0,
+	ERROR_LEXICAL = 1,			// chyba v programu v ramci lexikalni analyzy
+	ERROR_SYNTAX = 2,			// chyba v programu v ramci syntakticke analyzy
+	ERROR_UNDEFINED = 3, 			// semanticka chyba, nedefinovana promenna nebo funkce
+	ERROR_REDEFINITION = 3,			// semanticka chyba, redefinice promenne nebo funkce
+	ERROR_TYPE_INFERENCE = 4,		// semanticka chyba pri odvozovani datového typu nove definovane promenne
+	ERROR_TYPE_COMPATIBILITy = 5,		// semanticka chyba typove kompatibility ve vyrazech
+	ERROR_PARAMETERS = 6,			// semanticka chyba poctu nebo typu parametru
+	ERROR_RETURN_VALUE = 6, 		// semanticka chyba navratovych hodnot u funkci
+	ERROR_SEMANTICS = 7,			// jine semanticke chyby
+	ERROR_ZERO_DIVISION = 9,		// semanticka chyba deleni nulou
+	ERROR_COMPILER = 99,			// interni chyba prekladace (alokace pameti, ...)
 } ERROR_CODE;
 
 #endif
