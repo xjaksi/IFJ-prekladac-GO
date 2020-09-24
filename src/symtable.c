652385a380a1cdaaed59126ae66a5d7166cae532
xjaksi <xjaksi@users.noreply.github.com>
2020-09-24 08:55:01 +0200

    prvni verze symtable

---
+++ src/symtable.c
@@ -0,0 +1,175 @@
+/* ----------------------------------------------
+	Předmět: IFJ
+	Tým: 
+	Autor:
+		Jaksik, Ales (xjaksi01)
+-------------------------------------------------
+        Vlasakova, Nela (xvlasa14)
+		Belohlavek, Honza
+-------------------------------------------------
+ Datum: 24.9. 2020
+ Účel: Tabulka symbolů
+ Popis: Tabulka symbolů
+ ----------------------------------------------*/
+
+#include "symtable.h"
+
+/* ---------------------------------------------------- */
+/*                    Binarni strom                     */
+
+
+/**
+ * Inicializace stromu
+ */
+void BSTInit (tBSTNodePtr *RootPtr)
+{
+	*RootPtr = NULL; //inicializace prazdneho uzlu
+}
+
+/**
+ * Vyhledani uzlu
+ *
+ * Funkce prochazi strom
+ * pokud jsem nasel klic vratim obsah uzlu
+ * jinak vracim NULL
+ */
+nodeInfCont BSTSearch (tBSTNodePtr *RootPtr, char* K)
+{
+	// pokud je root prazdny
+	if (*RootPtr == NULL)
+	{
+		return NULL;
+	}
+
+	// pokud kodnota klice odpovida hledanemu vratim ho
+	else if (strcmp(K, (*RootPtr)->Key) == 0)
+	{
+		return (*RootPtr)->TBSNodeCont;
+	}
+
+	// v jinem pripade cestuji stromem a hledam dale
+
+	// pokud je hodnota klice mensi nez aktualni root
+	else if (strcmp(K, (*RootPtr)->Key) < 0)
+	{
+		// volam funkci rekurzivne a posunu se do leveho podstromu
+		return BSTSearch(&(*RootPtr)->LPtr, K);
+	}
+	else
+	{
+		// jinak priste pokracuji v pravem podstromu
+		return BSTSearch(&(*RootPtr)->RPtr, K);
+	}
+}
+
+/**
+ * Pridani uzlu
+ *
+ * Funkce prida uzel na spravne misto ve strome
+ * Pokud je jiz misto plne aktualizujeme hodnotu
+ *
+ * Posouvam se pomoci cyklu, abych nezatezoval pamet a zasobnik
+ */
+int BSTInsert (tBSTNodePtr* RootPtr, char* K, nodeInfCont Content)
+{
+	// pokud je strom prazdny vytvorime novy
+	if (*RootPtr == NULL)
+	{
+		// alokace mista pro koren
+		*RootPtr = (tBSTNodePtr) malloc(sizeof(struct tBSTNode));
+		if (*RootPtr == NULL)
+		{
+			return ERROR_COMPILATOR;
+		}
+
+		// vlozeni hodnot
+		(*RootPtr)->Key = K;
+		(*RootPtr)->TBSNodeCont = Content;
+		(*RootPtr)->LPtr = NULL;
+		(*RootPtr)->RPtr = NULL;
+
+		return NOERROR_OK;
+	}
+
+	// pokud se klice shoduji, redefinace odchazim
+	else if (strcmp(K, ((*RootPtr)->Key)) == 0)
+	{
+		return ERROR_SEM_VAR;
+	}
+
+	// v jinem pripade se posunuji stromem
+
+	// pokud je klic mensi nez aktualni root
+	else if (strcmp(K, ((*RootPtr)->Key)) < 0)
+	{
+		// opetovne volam funkci pro levy podstrom
+		BSTInsert(&((*RootPtr)->LPtr), K, Content);
+		return NOERROR_OK;
+	}
+
+	// v jinem pripade jdu v pravo
+	else
+	{
+		BSTInsert(&((*RootPtr)->RPtr), K, Content);
+		return NOERROR_OK;
+	}
+}
+
+
+/**
+ * Zniceni stromu
+ */
+void BSTDispose (tBSTNodePtr *RootPtr)
+{
+	// pokud neni strom prazdny nicim
+	if (*RootPtr != NULL)
+	{
+		// projdeme strom z leva i prava
+		BSTDispose(&((*RootPtr)->LPtr));
+		BSTDispose(&((*RootPtr)->RPtr));
+
+		// uvolnovani pameti
+		free(*RootPtr);
+
+		// po inicializaci je NULL
+		*RootPtr = NULL;
+	}
+}
+
+/**
+ * Vytvareni obsahu
+ * nahraje do data obsah pro koren
+ */
+nodeInfCont createCont (nodeType nnType, int noParam, bool isDefined, dataType ddType)
+{
+    nodeInfCont data = malloc(sizeof(struct nodeCont));
+    data->nType = nnType;
+    data->noParams = noParam;
+    data->defined = isDefined;
+    data->dType = ddType;
+
+    return data;
+}
+
+/**
+ * Aktualizace datoveho typu, poctu parametru
+ */
+void BSTContActual (tBSTNodePtr *RootPtr, char* K, dataType type, int params)
+{
+	// podle klice najdeme poren v danem binarnim stromu
+	nodeInfCont upDate = BSTSearch(RootPtr, K);
+
+	upDate->dType = type;
+	upDate->noParams = params;
+}
+
+/*                Konec binarniho stromu                */
+/* ---------------------------------------------------- */
+
+
+/* ---------------------------------------------------- */
+/*                 Vypisovani pro testy                 */
+
+
+/*                Konec kontrolnich vypisu              */
+/* ---------------------------------------------------- */
---
+++ src/symtable.h
@@ -0,0 +1,77 @@
+/* ----------------------------------------------
+	Předmět: IFJ
+	Tým: 
+	Autor:
+		Jaksik, Ales (xjaksi01)
+-------------------------------------------------
+        Vlasakova, Nela (xvlasa14)
+        Belohlavek, Honza
+-------------------------------------------------
+ Datum: 24.9. 2020
+ Účel: Tabulka symbolů
+ Popis: Hlavičkový soubor pro tabulku symbolů
+ ----------------------------------------------*/
+
+#ifndef SYMTABLE_H
+#define SYMTABLE_H
+
+// nejake knihovny
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <string.h>
+#include "errors.h"
+
+/* ---------------------------------------------------- */
+/*         Pomucky pro praci s binarnim stromem         */
+
+// typ uzlu
+typedef enum
+{
+    ntVar,          // jedna se o promennou
+    ntFunc          // koren je funkce
+} nodeType;
+
+// datove typy
+typedef enum datatype
+{
+    typeNo,
+    typeInt,
+    typeFloat,
+    typeString,
+} dataType;
+
+// informace o obsahu uzlu
+typedef struct nodeCont
+{
+    nodeType nType;                 // jedna se o funkci nebo promennou
+    int noParams;                   // pocet parametru teto funkce
+    bool defined;                   // bylo jiz definovano?
+    dataType dType;                 // nazev datoveho typu
+} *nodeInfCont;
+
+
+// struktura samotneho uzlu
+typedef struct tBSTNode
+{
+	char * Key;			            // klic/nazev promenne
+	struct nodeCont * TBSNodeCont;  // informace o uzlu
+	struct tBSTNode * LPtr;         // levy podstrom
+	struct tBSTNode * RPtr;         // pravy podstrom
+} *tBSTNodePtr;
+
+// prototypy funkci
+
+void BSTInit   (tBSTNodePtr *);
+nodeInfCont BSTSearch  (tBSTNodePtr *, char *);
+int BSTInsert (tBSTNodePtr *, char *, nodeInfCont);
+void BSTDispose(tBSTNodePtr *);
+
+nodeInfCont createCont (nodeType, int, bool, dataType);
+void BSTContActual (tBSTNodePtr *, char*, dataType, int);
+
+/*                Konec binarniho stromu                */
+/* ---------------------------------------------------- */
+
+#endif
