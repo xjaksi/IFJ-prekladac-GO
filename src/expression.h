1b447578c7aba83e872253c16d9b82e09165c9b4
xvlasa14 <48327260+xvlasa14@users.noreply.github.com>
2020-09-24 09:59:54 +0200

    Create expression.h
    vytvoreni souboru

---
+++ src/expression.h
@@ -0,0 +1,9 @@
+/* ----------------------------------------------
+          IFJ prekladac jazyka IFJ20
+	Tým XX:
+		Jaksik, Ales (xjaksi01)
+		Vlasakova, Nela (xvlasa14)
+		Belohlavek, Jan (xbeloh8)
+-------------------------------------------------
+Ucel: deklarace funkci
+----------------------------------------------*/
