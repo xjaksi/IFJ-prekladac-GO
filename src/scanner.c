d74e65bfaa83b5eb955d15350c154e7798b0cbab
XipCZ1 <55874831+XipCZ1@users.noreply.github.com>
2020-10-28 14:58:38 +0100

    Create scanner.c

---
+++ src/scanner.c
@@ -0,0 +1,10 @@
+/* ----------------------------------------------
+ 	IFJ prekladac jazyka IFJ20
+	Tým XX:
+		Jaksik, Ales (xjaksi01)
+		Vlasakova, Nela (xvlasa14)
+		Belohlavek, Jan (xbeloh8)
+-------------------------------------------------
+Ucel: co dela tento soubor
+Popis:  jak funguje tento soubor
+----------------------------------------------*/ 
