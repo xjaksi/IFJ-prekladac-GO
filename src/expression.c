bbe6617594a303686f69549f24afed85d47009b2
xvlasa14 <nel.vlasakova@gmail.com>
2020-10-31 19:06:25 +0100

    Update expression.c

--- src/expression.c
+++ src/expression.c
@@ -1,13 +1,15 @@
-/* ----------------------------------------------
- 	IFJ prekladac jazyka IFJ20
-	Tým 101:
-		Vlasakova, Nela (xvlasa14)
-		Jaksik, Ales (xjaksi01)
-		Belohlavek, Jan (xbeloh8)
-		Mraz, Filip (xmrazf00)
--------------------------------------------------
-Ucel: precedencni analyza vyrazu
-Popis:  TODO
-----------------------------------------------*/
+/* ------------------------------------------------
+ * @file expression.c
+ *	IFJ prekladac jazyka IFJ20
+ *	Tým 101:
+ *		@author Vlasakova, Nela <xvlasa14>
+ * ------------------------------------------------
+ *		@author Jaksik, Ales <xjaksi01>
+ *		@author Belohlavek, Jan <xbeloh8>
+ *		@author Mraz, Filip <xmrazf00>
+ * ------------------------------------------------
+ * @brief precedencni analyza vyrazu
+ * -----------------------------------------------*/ 
 
 #include expression.h
+
