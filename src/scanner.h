c7303de57772d670999796f51d9c2cfc0581dde9
xvlasa14 <nel.vlasakova@gmail.com>
2020-11-05 21:04:15 +0100

    opraveny různé věci

--- src/scanner.h
+++ src/scanner.h
@@ -15,56 +15,47 @@ Popis:  jak funguje tento soubor
  * @brief Typy tokenu a klicova slova
  */
 typedef enum {
-	tID,		///< typ ID 
-	tINT,		///< typ integer
-	tFLOAT,		///< typ float
-	tSTRING,	///< typ string
-	tADD,		///< +
-	tSUB,		///< -
-	tDIV,		///< /
-	tMUL,		///< *
-	tASSIGN,	///< :=
-	tLT,		///< <
-	tLEQ,		///< <=
-	tGT,		///< >
-	tGEQ,		///< >=
-	tEQ,		///< ==
-	tNEQ,		///< !=
-	tLBRACKET,	///< (
-	tRBRACKET,	///< )
-	tLBRACE,	///< {
-	tRBRACE,	///< }
-	tSEMICOLON,	///< ;
-	tDEVNULL,	///< _
+	tID,		// typ ID 
+	tINT,		// integer
+	tFLOAT,		// float
+	tSTRING,	// string
+	tADD,		// +
+	tSUB,		// -
+	tDIV,		// /
+	tMUL,		// *
+	tASSIGN,	// :=
+	tLT,		// <
+	tLEQ,		// <=
+	tGT,		// >
+	tGEQ,		// >=
+	tRBRACKET,	// (
+	tLBRACKET,	// )
+	tRBRACE,	// {
+	tLBRACE,	// }
+	tSEMICOLON,	// ;
+	tDEVNULL,	// _
 
 	// klicove slova
-	kwIF,		///< if
-	kwELSE,		///< else
-	kwFOR,		///< for
-	kwRETURN,	///< return
-	kwFLOAT64,	///< float64
-	kwFUNC,		///< func
-	kwSTRING,	///< string
-	kwINT,		///< int
+	kwIF,		
+	kwELSE,
+	kwFOR,
+	kwRETURN,
+	kwFLOAT64,
+	kwFUNC,
+	kwSTRING,
+	kwINT
 
-	// vestavene funkce
-	fINPUTS,	///< inputs
-	fINPUTI,	///< inputi
-	fINPUTF,	///< inputf
-	fPRINT,		///< print
-	fINT2FLOAT, ///< int2float
-	fFLOAT2INT, ///< float2int
-	fLEN,		///< len
-	fSUBSTR,	///< substr
-	fORD,		///< ord
-	fCHR,		///< chr
-
-	fMAIN		///< main
 } TokenType;
 
-typedef struct s_token {
+/**
+ * @struct struktura pro token
+   @brief obsahuje udaje o tokenu a ukazatele na praveho a leveho souseda
+ */
+typedef struct Token{
 	TokenType type;
 	char value[50];
-} s_token;
+	struct Token *lptr;
+	struct Token *rptr;
+} token;
 
 void get_token();
