5ed5cb2e86f8e94042ba164e3c9466682b6e1256
Diktator6 <45770139+Diktator6@users.noreply.github.com>
2020-11-11 10:18:02 +0100

    Add files via upload
    ten atribute tam bude dynamicky, ne polem

---
+++ src/tokenList.c
@@ -0,0 +1,167 @@
+/** -----------------------------------------------
+ * @file tokenList.c
+ *	IFJ prekladac jazyka IFJ20
+ *	Tým 101:
+ *		@author Jaksik, Ales <xjaksi01>
+ * ------------------------------------------------
+ *		@author Vlasakova, Nela <xvlasa14>
+ *		@author Belohlavek, Jan <xbeloh8>
+ *		@author Mraz, Filip <xmrazf00>
+ * ------------------------------------------------
+ * @brief Implementovane funkce pro praci s obousmerne vazanym seznamem tokenu
+ * -----------------------------------------------*/
+
+#ifndef TOKEN_LIST_C
+#define TOKEN_LIST_C
+
+#include<stdio.h>
+#include<stdlib.h>
+#include "tokenList.h"
+
+int solved;
+int errflg;
+
+/*
+** Vytiskne upozornění na to, že došlo k chybě.
+**/	
+void DLError() {        // TODO: interi chyba(allokace pameti), melo by to disposnout list a vratit nejake ernum
+
+    // printf ("*ERROR* The program has performed an illegal operation.\n");
+    // errflg = TRUE;             /* globální proměnná -- příznak ošetření chyby */
+    return;
+}
+
+void DLInitList (tokenList *L) {
+   
+    // nastavime vsechny pointry na NULL
+    L->First = NULL;
+    L->Act = NULL;
+    L->Last = NULL;
+}
+
+/*
+** Zruší všechny prvky seznamu L a uvede seznam do stavu, v jakém
+** se nacházel po inicializaci. Vsechny prvky uvolni funkci free().
+**/
+void DLDisposeList (tokenList *L) {
+   
+    // pomocny ukazatel na strukturu prvku
+    TokenPtr sup;
+
+    // cyklus rozkladajici seznam prvek po prvku
+    while(L->First != NULL){
+        sup = L->First->rptr;
+        free(L->First);
+        L->First = sup;
+    }
+    
+    // nastavime pointry Act a Last na NULL
+    L->Act = NULL;
+    L->Last = NULL;
+}
+
+
+void DLInsertLast(tokenList *L, TokenType param_t_type, char param_atribute[]) {
+/*
+** Vloží nový prvek na konec seznamu L (symetrická operace k DLInsertFirst).
+** V případě, že není dostatek paměti pro nový prvek při operaci malloc,
+** volá funkci DLError().
+**/
+    
+    // pomocny pointer ukazuje na prave vytvoreny novy prvek pomoci malloc()
+    TokenPtr sup = malloc(sizeof(struct Token));
+
+    // kontrola spravne allokace
+    if(sup == NULL){
+        DLError();
+        return;
+    }
+
+    // prirazeni hodnoty do noveho prvku
+    sup->*atribute = param_atribute;
+    sup->t_type = param_t_type;
+    sup->rptr = NULL;
+    sup->lptr = L->Last;
+
+    // pokud nevkladame do prazdneho seznamu, napoji se predchozi prvek na prvek pridavany 
+    if(sup->lptr != NULL){
+        L->Last->rptr = sup;
+    }
+    else{ // pokud se jedna o prvvni pridany prvek do seznamu nastavime First na sup
+        L->First = sup; 
+    }
+   
+    // aktualizce posledniho prvku na prvek pridavany
+    L->Last = sup;
+}
+
+/*
+** Nastaví aktivitu na první prvek seznamu L.
+**/
+void DLFirst (tokenList *L) {
+    L->Act = L->First;
+}
+
+/*
+** Nastaví aktivitu na poslední prvek seznamu L.
+**/
+void DLLast (tokenList *L) {	
+    L->Act = L->Last;	
+}
+
+
+/*
+** Prostřednictvím parametru val vrátí hodnotu aktivního prvku seznamu L.
+** Pokud seznam L není aktivní, volá funkci DLError ().
+**/
+void DLCopy (tokenList *L, int *param_t_type, char *param_atribute[]) {
+	
+    // kontrola aktivitty
+    if(L->Act == NULL){
+        DLError();
+        return;
+    }	
+
+    param_atribute = L->Act->atribute;
+    *param_t_type = L->Act->t_type;
+}
+
+/*
+** Posune aktivitu na následující prvek seznamu L.
+** Není-li seznam aktivní, nedělá nic.
+**/
+void DLSucc (tokenList *L) {
+
+    // kontrola aktivity
+    if(L->Act == NULL){
+        return;
+    }
+
+    L->Act = L->Act->rptr;
+}
+
+/*
+** Posune aktivitu na předchozí prvek seznamu L.
+** Není-li seznam aktivní, nedělá nic.
+**/
+void DLPred (tokenList *L) {
+
+    // kontrola aktivity
+    if(L->Act == NULL){
+        return;
+    }
+
+    // presunuti aktivity na levy prvek
+    L->Act = L->Act->lptr;
+}
+
+/*
+** Je-li seznam L aktivní, vrací nenulovou hodnotu, jinak vrací 0.
+**/
+int DLActive (tokenList *L) {
+
+    // ternarni operator priradi 0 kdyz seznam neni aktivni jinak 1 kdyz je
+    return (L->Act == NULL)? 0: 1;
+}
+
+#endif //TOKEN_LIST_C
