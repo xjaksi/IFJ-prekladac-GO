3f40a30fe1ce327d5191bf5750c605683d73f4ce
XipCZ1 <55874831+XipCZ1@users.noreply.github.com>
2020-11-11 19:16:31 +0100

    Update scanner.c

--- src/scanner.c
+++ src/scanner.c
@@ -12,21 +12,22 @@ Popis:  jak funguje tento soubor
 #include <stdio.h>
 #include <string.h>
 
+#inslude "tokenList.c" // funkce pro praci s tokenlistem
+#inslude "tokenList.c" // protototypy tokenlistu, tokenu, tokentype
 #include "scanner.h"
 #include "errors.h"
 
 int main(){
 
-	tListPtr tListMain = initScanner();// vytvoří obousměrné vázány list pro tokeny
+	tListPtr tListMain = initTokensList();// vytvoří obousměrné vázány list pro tokeny
 
-	get_token(tListMain); // naplni obousmerne vazany seznam tokeny z stdin
+	getTokensTo(tListMain); // naplni obousmerne vazany seznam tokeny z stdin
 	//printf("%s\n", tListMain->act->value);
 return 0;
 }
 
 
-
-tListPtr initScanner(){
+tListPtr initTokensList(){
 
 	tListPtr tListMain = malloc(sizeof(struct tList)); // hlavni list
 	tListMain->first = NULL;
@@ -37,43 +38,7 @@ tListPtr initScanner(){
 	return tListMain;
 }
 
-void tListAdd(tListPtr atr, TokenType type, char valu[]){
-
-	TokenPtr newItem = malloc(sizeof(struct Token));//zalozi novy prvek listu
-  	if (newItem == NULL) {
-    	return ERROR_COMPILER;
-   	}
-
-
-	if(atr->numberTokensInList == 0){
-		atr->first = newItem;
-		atr->act = newItem;
-		atr->last = newItem;
-		atr->numberTokensInList++;
-		newItem->type = type;//uloži do nej hodnoty
-		newItem->value[1000000] = valu;
-		newItem->next = NULL;
-		newItem->prev = NULL;
-		return 0;
-	}
-
-
-
-	newItem->prev = atr->act; // nastaveni predchoziho prvku pro momentalni prvek
-	atr->act->next = newItem; // nastaveni nasledujiciho prvku predchozimu prvku
-	newItem->next = NULL; // nastaveni nasledujiciho prvku
-	atr->act = newItem; //aktualizace act prvku v seznamu 
-	atr->last = newItem; // TODO: dat to jednou na konec 
-	atr->numberTokensInList++;
-	newItem->type =type;//uloži do nej hodnoty
-	newItem->value[1000000] = valu;
-
-	return 0;
-}
-
-
-
-void get_token(tListPtr tListMain){ //fuknce pro precteni dat ze std. vstupu a ulozeni do seznamu , DKA
+void getTokensTo(tListPtr tListMain){ //fuknce pro precteni dat ze std. vstupu a ulozeni do seznamu , DKA
 
 	char tmpString[] = "";
 
@@ -84,11 +49,7 @@ void get_token(tListPtr tListMain){ //fuknce pro precteni dat ze std. vstupu a u
 
 	printf("%s\n", tmpString);
 		
-	tListAdd(tListMain, tID, tmpString);
+	tListAdd(tListMain, tID, tmpString);//ulozit do oboustranne vazaneho seznamu
 
-	//ulozit do oboustranne vazaneho seznamu
-
-
-
-return 0;
+	return 0;
 }
