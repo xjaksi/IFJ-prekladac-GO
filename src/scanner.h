8d53c4a26ea7b3f14b5a9cf85e27b5ca8049f480
XipCZ1 <55874831+XipCZ1@users.noreply.github.com>
2020-11-11 19:16:28 +0100

    Update scanner.h

--- src/scanner.h
+++ src/scanner.h
@@ -10,83 +10,10 @@ Popis:  jak funguje tento soubor
 ----------------------------------------------*/
 #include <stdlib.h>
 
+// TokenType - definovan v tokenList.h
+// *TokenPtr definovan v tokenList.h
+// tokenList definovan v tokenList.h
 
-/**
- * @brief Typy tokenu a klicova slova
- */
-typedef enum {
-	tID,		///< typ ID
-	tINT,		///< typ integer
-	tFLOAT,		///< typ float
-	tSTRING,	///< typ string
-	tADD,		///< +
-	tSUB,		///< -
-	tDIV,		///< /
-	tMUL,		///< *
-	tDEF,		///< :=
-	tASSIGN,	///< =
-	tLT,		///< <
-	tLEQ,		///< <=
-	tGT,		///< >
-	tGEQ,		///< >=
-	tEQ,		///< ==
-	tNEQ,		///< !=
-	tLBRACKET,	///< (
-	tRBRACKET,	///< )
-	tLBRACE,	///< {
-	tRBRACE,	///< }
-	tSEMICOLON,	///< ;
-	tDEVNULL,	///< _
-	tEOL,		///< EOL
-	tEOF,		///< EOF
-
-	// klicove slova
-	kwIF,		///< if
-	kwELSE,		///< else
-	kwFOR,		///< for
-	kwRETURN,	///< return
-	kwFLOAT64,	///< float64
-	kwFUNC,		///< func
-	kwSTRING,	///< string
-	kwINT,		///< int
-	kwPACKAGE,	///< package
-
-	// vestavene funkce
-	fINPUTS,	///< inputs
-	fINPUTI,	///< inputi
-	fINPUTF,	///< inputf
-	fPRINT,		///< print
-	fINT2FLOAT, ///< int2float
-	fFLOAT2INT, ///< float2int
-	fLEN,		///< len
-	fSUBSTR,	///< substr
-	fORD,		///< ord
-	fCHR,		///< chr
-
-	fMAIN		///< main
-} TokenType;
-
-/**
- * @struct token reprezentujici jednu polozku v obousmerne vazanem seznamu
- * @brief obsahuje odkaz na predesly a nasledujici
- */
-typedef struct Token {
-	TokenType type;
-	char value[1000000];
-	struct Token *next;
-	struct Token *prev;
-} *TokenPtr;
-
-/**
- * @struct struktura reprezentujici boboustranne vazany seznam
- * @brief obsahuje první, poslední a aktivní prvek
- */
-typedef struct tList{
-	TokenPtr first;
-	TokenPtr act;
-	TokenPtr last;
-	int numberTokensInList;
-} *tListPtr;
-
-void get_token(tListPtr tListMain);
-tListPtr initScanner();
+//prototypy funkci
+tListPtr initTokensList();
+void getTokensTo(tListPtr tListMain);
