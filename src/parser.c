6f204a12e6997b4f5133b734de8e2b6c0e22170b
xjaksi <xjaksi@users.noreply.github.com>
2020-12-05 12:28:48 +0100

    Update parser.c

--- src/parser.c
+++ src/parser.c
@@ -655,6 +655,7 @@ int cAssign(tokenList *token, treeNode *funcTab, treeList *tList, int item)
     // nahrani datovych typu
     for (int i = 0; i < item; i++)
     {
+        if (token->Act->t_type == tEOL) return ERROR_SEMANTICS;
         result = cExpr(token, tList, &type);
         // // fprintf(stderr, "CHECK 10 %d result: %d\n", type, result); 
         if (result != OK) return result;
